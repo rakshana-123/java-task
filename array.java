49faef3c51207b1b6c37bd0c7e76438e14e7bc4f
rakshana-123 <raks061128@gmail.com>
2026-02-24 18:51:24 +0530

    functionality to array

--- array.java
+++ array.java
@@ -35,4 +35,81 @@ Enter array elements
 22 33 45 67
 the max element is:67
 
+#search array
+
+package rakshana;
+import java.util.Scanner;
+public class array {
+	int search(int a[],int key)
+	{
+		  for(int i=0;i<a.length;i++)
+		  {
+        	  if(key==a[i])
+        	  {
+                     return i;
+        	  }
+		  }
+		  return -1;
+		  }
+	 
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the size of the array:");
+		int size=sc.nextInt();
+		int a[]=new int[size];
+		System.out.println("Enter the elements:");
+		for (int i=0;i<a.length;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+        System.out.println("array elements are");
+          for(int i=0;i<a.length;i++)
+          {
+        	  System.out.println(a[i]);
+        	  
+          }
+          System.out.println("enter the search element:");
+          int key=sc.nextInt();
+          array s=new array();
+          int x=s.search(a, key);
+        		  if(x==-1)
+        		  {
+        			  System.out.println("element not found");
+
+        		  }
+        		  else
+        		  {
+        			  System.out.println("element found "+x);
+        		  }
+          
+        	}
+}
+	
+output:
+enter the size of the array:
+5
+Enter the elements:
+11
+22
+33
+44
+55
+array elements are
+11
+22
+33
+44
+55
+enter the search element:
+44
+element found 3
+
+	
+	
+	
+	
+
+	  
+	
+
   
